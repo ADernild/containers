c9f4279545ce74bedd2adf9b622c0c56ade9eed8
Alexander Dernild <aid@sdu.dk>
2023-03-21 15:37:06 +0100

    Added -y flag as we want it to run without interaction

--- pgbouncer/init.sh
+++ pgbouncer/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -15,6 +15,6 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run --name pgbouncer bitnami/pgbouncer:latest
