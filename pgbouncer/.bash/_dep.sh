ea5e0192081a3b2b4862621b9b6f96e0e9b819c3
Alexander Dernild <aid@sdu.dk>
2023-03-29 10:33:27 +0200

    deleted unused init.sh

---
+++ pgbouncer/.bash/_dep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Adding PGDG apt repository
+
+wget -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+
+
+sudo su -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main' >> /etc/apt/sources.list.d/pgdg.list"
+
+sudo apt-get update && \
+sudo apt-get install -y pgdg-keyring
+sudo apt-get install -y pgbouncer
